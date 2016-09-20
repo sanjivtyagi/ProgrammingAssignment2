10550db32f64dda40f6ff355103c896fc8b1315f
Sanjiv <t_sanjiv@yahoo.com>
2016-09-20 10:54:28 -0700

    This is week 3 assignment
    This is week 3 assignment

--- cachematrix.R
+++ cachematrix.R
@@ -5,15 +5,20 @@
 
 makeCacheMatrix <- function(x = matrix()) {
  
- inv <- NULL
+ inv = NULL
+ # set vaiable
     set <- function(y) {
         x <<- y
         inv <<- NULL
     }
+ # get variable
     get <- function() x
-    setinverse <- function(inverse) inv <<- inverse
-    getinverse <- function() inv
-    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
+ # set inverse matrix
+    setinv <- function(inverse) inv <<- inverse
+ # get inverse matrix
+    getinv <- function() inv
+ # put all values in list
+    list(set=set, get=get, setinv=setinv, getinv=getinv)
 
 }
 
@@ -21,14 +26,16 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        inv <- x$getinverse()
-    if(!is.null(inv)) {
+     ## Return a matrix that is the inverse of 'x'
+        inv = x$getinv()
+ # it returned caching matrix    
+        if(!is.null(inv)) {
         message("getting cached data.")
         return(inv)
     }
-    data <- x$get()
-    inv <- solve(data)
-    x$setinverse(inv)
-    inv
+ # calclulate inverse matrix   
+    matdata <- x$get()
+    inv <- solve(matdata)
+    x$setinv(inv)
+    return(inv)
 }
